9abddaa3dbb6a64093debd6b276d4f12aa70bbd9
Aria Moradi <aria.moradi007@gmail.com>
2025-03-22 23:44:07 +0330

    fix some layout

--- src/config/menu.ts
+++ src/config/menu.ts
@@ -7,20 +7,20 @@ export const links: MainNavItem[] = [
     title: 'Post',
     href: `${base}/post/`
   },
-  {
-    title: 'Short',
-    href: `${base}/short/`
-  },
-  {
-    title: 'Projects',
-    href: `${base}/projects/`
-  },
-  {
-    title: 'Uses',
-    href: `${base}/uses/`
-  },
-  {
-    title: 'Collective',
-    href: `${base}/collective/`
-  }
+  // {
+  //   title: 'Short',
+  //   href: `${base}/short/`
+  // },
+  // {
+  //   title: 'Projects',
+  //   href: `${base}/projects/`
+  // },
+  // {
+  //   title: 'Uses',
+  //   href: `${base}/uses/`
+  // },
+  // {
+  //   title: 'Collective',
+  //   href: `${base}/collective/`
+  // }
 ]
