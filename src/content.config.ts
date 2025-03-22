aa803623c31eac62f1b7c7e7e823495c824bee1f
Aria Moradi <aria.moradi007@gmail.com>
2025-03-23 00:06:53 +0330

    make it better loking

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,14 +2,14 @@ import { glob } from 'astro/loaders'
 import { defineCollection, z } from 'astro:content'
 
 export const post = defineCollection({
-  loader: glob({ pattern: '**\/[^_]*.mdx', base: './src/content/post' }),
+  loader: glob({ pattern: '**\/*.mdx', base: './src/content/post' }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
-      description: z.string(),
+      description: z.string().optional(),
       cover: image().optional(),
       coverAlt: z.string().optional(),
-      category: z.string().array(),
+      category: z.string().array().optional(),
       pubDate: z
         .string()
         .or(z.date())
