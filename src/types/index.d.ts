70be1f776fba9c8151cb82ac4eb7269f2eeceb9d
Aria Moradi <aria.moradi007@gmail.com>
2025-03-23 13:58:39 +0330

    fix index

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -27,29 +27,3 @@ export interface SiteConfig {
     displayNumber: number
   }
 }
-
-interface CommonItem {
-  title: string
-  icon?: ''
-  url?: string
-  desc?: string
-}
-
-export type UsingItem = CommonItem
-export type CreditItem = CommonItem
-
-export interface CollectiveItem {
-  name: string
-  url: string
-  desc?: string
-  tags?: string[]
-}
-
-export interface ProjectItem {
-  title: string
-  state: 'InProgress' | 'Fail' | 'Success'
-  url: string
-  icon?: string
-  desc?: string
-  tags?: string[]
-}
