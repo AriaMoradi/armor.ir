43ca36b1aa85487bb8e66521873ac613219554a0
Aria Moradi <aria.moradi007@gmail.com>
2025-03-23 16:15:33 +0330

    cleanup

--- src/lib/fetchers.ts
+++ src/lib/fetchers.ts
@@ -15,30 +15,6 @@ export async function getPosts() {
   return posts
 }
 
-export async function getShorts() {
-  return (await getCollection('short')).sort(
-    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
-  )
-}
-
-export async function getShortCategories() {
-  const shorts = await getCollection('short')
-  return [
-    ...new Set(shorts.map(short => short.data.category).flat()),
-  ]
-}
-
-export async function getShortsByYear(year: string) {
-  const posts = (await getCollection('short'))
-    .sort(
-      (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
-    )
-    .filter((ele) => {
-      return ele.id.substring(0, 4) === year.toString()
-    })
-  return posts
-}
-
 export async function getPostsByCategory(category: string) {
   const posts = (await getCollection('post'))
     .filter(post => post.data.category.includes(category))
